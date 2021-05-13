4cfc580dd78fedcd9d57e78dff1960774ffc2b50
Micaela <76819539+mikaasf@users.noreply.github.com>
2021-05-13 18:14:16 +0100

    Import and retrieve videos

--- server_pages.py
+++ server_pages.py
@@ -1,15 +1,33 @@
+import mimetypes
 import re
 import secrets
+from time import time
 from urllib.parse import urlparse, urljoin
+import os
+from zlib import adler32
 
 import cv2
 import flask
-from flask import Flask, render_template, request, redirect, url_for, make_response, session
+import numpy as np
+from flask import Flask, render_template, request, redirect, url_for, make_response, session, Response, \
+    stream_with_context, send_from_directory, send_file, copy_current_request_context
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_socketio import SocketIO, emit
-from send_frame_handler import SendFrame
+from flask_socketio import SocketIO
+from zmq.utils.strtypes import unicode
 
+from send_frame_handler import SendFrame
+from datetime import datetime
+from werkzeug.datastructures import Headers
+import urllib
+
+
+UPLOAD_DIRECTORY = "./static/videos"
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
+ALLOWED_EXTENSIONS = {'mpeg', 'mp4'}
 # ==================================
 # ==== APP INITIALIZATION PARAMETERS ====
 app = Flask(__name__)
@@ -17,9 +35,12 @@ app.secret_key = secrets.token_bytes(16)
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'QueroPote2026*'
 app.config['MYSQL_DATABASE_DB'] = 'projeto'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# file upload configurations
+app.config['UPLOAD_FOLDER'] = UPLOAD_DIRECTORY
+app.config['MAX_CONTENT_LENGTH'] = 256 * 1024 * 1024  # 256 mb
 
 db = MySQL()
 db.init_app(app)
@@ -172,7 +193,7 @@ def signup():
                     register_user(username, password, email)
                     return log_the_user_in(username)
                 else:
-                    error = 'That e-mail or username are already registered'
+                    error = 'That e-mail or username is already registered'
             elif request.form['pass'] is not "":
                 error = 'Passwords don\'t match'
             if len(request.form['pass']) < 6:
@@ -202,40 +223,62 @@ def register_user(username, password, email):
 # ==================================
 # ==== MAIN PAGES METHODS ====
 
+def ext_generate():
+    global com_socket_handler
+    while True:
+        frame = com_socket_handler.get_buffer
+        if frame:
+            frame = com_socket_handler.get_buffer.pop()
+            if frame[0]:
+                img = cv2.imencode('.jpg', frame[1])[1].tobytes()
+                print(img)
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
 
-def _convert_image_to_jpeg(frame):
-    ret, buffer = cv2.imencode('.jpg', frame)
-    frame = buffer.tobytes()
-    return (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-
-def generate(debug=False):
-    print("VIDEO")
-    if debug:
-        cam = cv2.VideoCapture(0)
-        while True:
-            flag, frame = cam.read()
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+def ext_generate_cam():
+    global com_socket_handler
+    while True:
+        frame = com_socket_handler.get_buffer
+        if frame:
+            frame = com_socket_handler.get_buffer.pop()
+            img = cv2.imencode('.jpg', frame)[1].tobytes()
+            print(img)
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    else:
-        global com_socket_handler
-        if len(com_socket_handler.get_buffer) != 0:
-            print("yes")
-            frame = _convert_image_to_jpeg(com_socket_handler.get_buffer.pop())
-        else:
-            frame = cv2.imread("./static/imgs/eyeLogo.png")
-            frame = _convert_image_to_jpeg(cv2.resize(frame, (640, 480)))
-            print("no")
-        print(type(frame))
-        return frame
+                   b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
+
+
+def generate_test_img():
+    frame = cv2.imread("./static/imgs/eyeLogo.png")
+    frame = cv2.imencode('.jpg', frame)[1].tobytes()
+    print(type(frame))
+    return (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+# ---- PARA WEBCAM LOCAL -----
+def gen():
+    """Video streaming generator function."""
+    camera = cv2.VideoCapture(0)
+    while True:
+        flag, img = camera.read()
+        img = cv2.imencode('.jpg', img)[1].tobytes()
+        if flag:
+            print("a")
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
+
+
+@app.route('/video')
+def video():
+    return Response(ext_generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     global com_socket_handler
+    if com_socket_handler:
+        com_socket_handler.close_socket()
     if request.method == 'POST':
         vid_title = request.form['vid_title']
         # todo fetch & store extra parameters according to recorded video
@@ -243,8 +286,7 @@ def home():
         return redirect(url_for('after_recording'))
     if 'username' in session:
         if not com_socket_handler:
-            com_socket_handler = SendFrame(socketio)
-            com_socket_handler.daemon = True
+            com_socket_handler = SendFrame()
             com_socket_handler.start()
         return render_template("page.html", page='on_rec', name=get_user_data(
             session['username']))  # , img_test=_convert_image_to_jpeg(cv2.resize(img, (640, 480))))
@@ -286,21 +328,118 @@ def myvideos():
     if request.method == 'POST':
         return redirect(url_for('home'))
     if 'username' in session:
-        videos = execute_one_query("SELECT title, uploadDate FROM video WHERE username=%s", session['username'], True)
+        videos = execute_one_query("SELECT title, uploadDate, idVideo FROM video WHERE username=%s",
+                                   session['username'], True)
+        videos = np.asarray(videos)
+        for v in videos:
+            v[0] = urllib.parse.unquote(v[0])
+        videos = tuple(videos)
         return render_template('myvideos.html', page='myvideos', name=get_user_data(session['username']), videos=videos)
     else:
         return redirect(url_for('login'))
 
 
-@app.route('/after_recording', methods=['POST', 'GET'])
-def after_recording():
+@app.route('/import_video', methods=['GET', 'POST'])
+def import_video():
+    if request.method == 'POST':
+        if 'upFile' in request.files:
+            file = request.files['upFile']
+            filename = urllib.parse.quote(file.filename)
+            full_path = os.path.join(UPLOAD_DIRECTORY, filename)
+            if os.path.exists(full_path):
+                return make_response(('File already exists', 400))
+            if allowed_file(file.filename):
+                with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
+                    file.save(os.path.join(UPLOAD_DIRECTORY, filename))
+                    recTime = None
+                    location = None
+                    insert("INSERT INTO video values (%s, %s, %s, %s, %s, %s, %s)",
+                           [None, datetime.today().strftime('%Y-%m-%d-%H:%M:%S'), file.filename, session['username'],
+                            full_path, recTime, location])
+                    # Return 201 CREATED
+                    return redirect('myvideos', 201)
+        else:
+            redirect(url_for('importvideo'))
+    if 'username' in session:
+        return render_template('import_video.html', page='importvideo', name=get_user_data(session['username']))
+    else:
+        return redirect(url_for('login'))
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/after_recording/<idVideo>', methods=['POST', 'GET'])
+def after_recording(idVideo):
     if request.method == 'POST':
         # todo changes (extra parameters) according to selected video
         return redirect(url_for('home'))
     if 'username' in session:
-        return render_template('after_recording.html', page='pos_rec', name=get_user_data(session['username']))
+        video = execute_one_query(
+            "SELECT title, uploadDate, location, recTime, pathName FROM video WHERE username=%s AND idVideo=%s",
+            [session['username'], idVideo])
+        return render_template('after_recording.html', page='pos_rec', name=get_user_data(session['username']),
+                               video=video, idV=idVideo)
     else:
         return redirect(url_for('login'))
+
+
+def send_file_partial(path):
+    """
+        Simple wrapper around send_file which handles HTTP 206 Partial Content
+        (byte ranges)
+        (if it has any)
+    """
+    range_header = request.headers.get('Range', None)
+    if not range_header: return send_file(path)
+
+    size = os.path.getsize(os.path.abspath(path))
+    byte1, byte2 = 0, None
+
+    m = re.search('(\d+)-(\d*)', range_header)
+    g = m.groups()
+
+    if g[0]: byte1 = int(g[0])
+    if g[1]: byte2 = int(g[1])
+
+    length = size - byte1
+    if byte2 is not None:
+        length = byte2 - byte1
+
+    data = None
+    with open(path, 'rb') as f:
+        f.seek(byte1)
+        data = f.read(length)
+
+    rv = Response(data,
+                  206,
+                  mimetype=mimetypes.guess_type(path)[0],
+                  direct_passthrough=True)
+    rv.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
+    rv.set_etag('flask-%s-%s-%s' % (
+        os.path.getmtime(path),
+        os.path.getsize(path),
+        adler32(
+            path.encode('utf8') if isinstance(path, unicode)
+            else path
+        ) & 0xffffffff
+    ))
+    rv.headers.add('Cache-Control', 'no-cache')
+    return rv
+
+
+# fixme, not working
+@app.route('/get_video/<idVideo>', methods=['GET'])
+def get_video(idVideo):
+    path, name = execute_one_query("SELECT pathName, title FROM video WHERE username=%s AND idVideo=%s",
+                                   [session['username'], idVideo])
+
+    if "Range" not in request.headers:
+        return send_from_directory(app.config['UPLOAD_FOLDER'], urllib.parse.quote(name), conditional=True)
+
+    return send_file_partial(path)
 
 
 # ==================================
@@ -327,6 +466,12 @@ def connect_input(message):
     print("input", message['data'])
 
 
-if __name__ == "__main__":    
-        
-    socketio.run(app, debug=False, port=5001)
+# Handler for a message recieved over 'connect' channel
+@socketio.on('leaveRecording')
+def connect_input(message):
+    print("left Recording")
+
+
+if __name__ == "__main__":
+    # app.run(debug=True, port=5001, threaded=True)
+    socketio.run(app, debug=True, port=5003)
