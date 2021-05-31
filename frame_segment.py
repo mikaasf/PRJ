362314365cbbb09c058ca45269d52b7843c9bdf6
Miguel Peixoto <miguel.peixoto.99@gmail.com>
2021-05-31 17:25:42 +0100

    UDP datagram size bug fixed

--- frame_segment.py
+++ frame_segment.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 MAX_DGRAM: int = 2**16
-MAX_IMAGE_DGRAM: int = MAX_DGRAM - 64  # minus 64 bytes in case UDP frame overflown
+MAX_STREAM_DGRAM: int = MAX_DGRAM - 64  # minus 64 bytes in case UDP frame overflown
 
 
 class FrameSegment(object):
@@ -22,13 +22,16 @@ class FrameSegment(object):
         self.__addr: str = addr
             
     def send_frame(self, img: np.ndarray, audio: list):
+        # Calculate JSON payload structure size
         json_payload_size: int = len(json.dumps([1, None, 1, None]))
-        half_payload_size: int = (MAX_IMAGE_DGRAM + json_payload_size) // 2
+        half_payload_size: int = (MAX_STREAM_DGRAM + json_payload_size) // 2
         
+        # Convert bytes object to base64 object
         encoded_audio: str = base64.b64encode(b''.join(audio)).decode('utf-8')
         compress_img: bytes = cv2.imencode('.jpg', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))[1].tobytes()
         frame: str = base64.b64encode(compress_img).decode('utf-8')
 
+        # Calculate how many segments for image and audio
         video_size: int = len(frame)
         audio_size: int = len(encoded_audio)
 
@@ -40,56 +43,54 @@ class FrameSegment(object):
 
         while num_of_video_segments or num_of_audio_segments:
             
+            # Default video and audio payload size
             video_total_free_space: int = half_payload_size
             audio_total_free_space: int = half_payload_size
             
-            
+            # Maximize JSON payload size
             if num_of_audio_segments:
+                
                 if num_of_audio_segments == 1:
-                    print(num_of_audio_segments, (half_payload_size - audio_size - audio_array_pos_start))
                     video_total_free_space += half_payload_size - audio_size - audio_array_pos_start
                     
                 else:
-                    print(num_of_audio_segments)
                     video_total_free_space += 0
                 
             else:
-                print(num_of_audio_segments)
                 video_total_free_space += half_payload_size - len(json.dumps(None))
                 
                 
             if num_of_video_segments:
                 if num_of_video_segments == 1:
-                    print(num_of_video_segments, (half_payload_size - video_size - video_array_pos_start))
-                    audio_total_free_space += half_payload_size - video_size - video_array_pos_start
+                    remaining_video_size = video_size - video_array_pos_start
+                    audio_total_free_space += half_payload_size - remaining_video_size
                     
                 else:
-                    print(num_of_video_segments)
                     audio_total_free_space += 0
                 
             else:
-                print(num_of_video_segments)
                 audio_total_free_space += half_payload_size - len(json.dumps(None))
                     
                     
-            
+            # Update end segment pointer
             video_array_pos_end: int = min(video_size, video_array_pos_start + video_total_free_space)
             audio_array_pos_end: int = min(audio_size, audio_array_pos_start + audio_total_free_space)
 
+            # Wrap up all data
             data: list = [1 if video_array_pos_end == video_size else 0, 
                           frame[video_array_pos_start:video_array_pos_end] if num_of_video_segments else None,
                           1 if audio_array_pos_end == audio_size else 0,
                           encoded_audio[audio_array_pos_start:audio_array_pos_end] if num_of_audio_segments else None]
             
-            
+            # Dump data into JSON and send it
             data: str = json.dumps(data)
-            print("JSON payload size:", len(data))
             self.__s.sendto(bytes(data, encoding="utf-8"), (self.__addr, self.__port))
             
+            # Update start segment pointer
             video_array_pos_start = video_array_pos_end
             audio_array_pos_start = audio_array_pos_end
 
-
+            # Update the number of segments remain to send
             if num_of_video_segments > 0:
                 num_of_video_segments -= 1
                 
