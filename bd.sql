4495ebc53bb108e51ff5612d8ac848e7c6d19d12
Micaela <76819539+mikaasf@users.noreply.github.com>
2021-06-18 15:55:31 +0100

    Add files via upload

--- bd.sql
+++ bd.sql
@@ -1,7 +1,7 @@
 use projeto;
 SET GLOBAL max_allowed_packet = 64 * 1048576; # 64 MB
 
-drop table if exists person, video, annotation, videoAnnotation, frame;
+drop table if exists person, video, annotation, videoAnnotation, thumbnail, currentVideoID;
 
 CREATE TABLE IF NOT EXISTS person (
     username VARCHAR(20) PRIMARY KEY,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS person (
 ) ENGINE=InnoDB default CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS video (
-	idVideo int primary key auto_increment,
+	idVideo bigint primary key auto_increment,
     uploadDate DATETIME NOT NULL,
     title VARCHAR(70) NOT NULL,
     username VARCHAR(20) NOT NULL,
@@ -24,37 +24,53 @@ CREATE TABLE IF NOT EXISTS video (
         REFERENCES person (username)
 ) ENGINE=InnoDB default CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS frame (
-	idFrame int,
-    idVideo int,
-    image MEDIUMBLOB,
-    CONSTRAINT pk_frame PRIMARY KEY(idFrame, idVideo), 
+CREATE TABLE IF NOT EXISTS thumbnail (
+    idVideo int PRIMARY KEY,
+    imagePath VARCHAR(150) NOT NULL,
 	CONSTRAINT fk_frame FOREIGN KEY (idVideo)
         REFERENCES video (idVideo)
 ) ENGINE=InnoDB default CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS annotation (
     emotionType VARCHAR(50) primary key,
+    emotion boolean not null,
     icon MEDIUMBLOB
 ) ENGINE=InnoDB default CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS videoAnnotation (
     emotionType VARCHAR(50),
-	idFrame int,
+	idFrame bigint,
 	idVideo int,
     customText VARCHAR(80),
-	CONSTRAINT fk_videoAnnotation_frame FOREIGN KEY (idFrame, idVideo)
-        REFERENCES frame (idFrame, idVideo),
+    duration tinytext,
+	idAnnotation bigint PRIMARY KEY AUTO_INCREMENT,
+	# CONSTRAINT fk_videoAnnotation_frame FOREIGN KEY (idFrame, idVideo)
+       # REFERENCES frame (idFrame, idVideo),
 	CONSTRAINT fk_videoAnnotation_annotation FOREIGN KEY (emotionType)
         REFERENCES annotation (emotionType)
 ) ENGINE=InnoDB default CHARSET=utf8mb4;
 
-INSERT INTO annotation VALUES ("happy", Null);
-INSERT INTO annotation VALUES ("sad", Null);
-INSERT INTO annotation VALUES ("sleepy", Null);
-INSERT INTO annotation VALUES ("angry", Null);
-INSERT INTO annotation VALUES ("surprised", Null);
-INSERT INTO annotation VALUES ("thinking", Null);
-INSERT INTO annotation VALUES ("custom", Null);
-INSERT INTO annotation VALUES ("heartbeat", Null);
-INSERT INTO annotation VALUES ("other", Null);
+CREATE TABLE IF NOT EXISTS sensorData (
+    dataType VARCHAR(50),
+	idFrame bigint,
+	idVideo int,
+    valueData VARCHAR(80),
+    duration tinytext
+) ENGINE=InnoDB default CHARSET=utf8mb4;
+
+
+create table if not exists currentVideoID (
+ idVideo int
+);
+
+insert into currentvideoid set idVideo=(select max(idVideo) from video) + 1;
+
+INSERT INTO annotation VALUES ("happy", Null, 1);
+INSERT INTO annotation VALUES ("sad", Null, 1);
+INSERT INTO annotation VALUES ("sleepy", Null, 1);
+INSERT INTO annotation VALUES ("angry", Null, 1);
+INSERT INTO annotation VALUES ("surprised", Null, 1);
+INSERT INTO annotation VALUES ("thinking", Null, 1);
+INSERT INTO annotation VALUES ("custom", Null, 2);
+INSERT INTO annotation VALUES ("other", Null, 3);
+
