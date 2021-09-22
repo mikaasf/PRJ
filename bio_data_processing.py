0bf8b7c03ec81d2aadca3092df0d0ea292d6cd42
Miguel Peixoto <miguel.peixoto.99@gmail.com>
2021-09-22 17:53:41 +0100

    Acrescento da função calculate_bpm

--- bio_data_processing.py
+++ bio_data_processing.py
@@ -25,3 +25,15 @@ def ppg_signal_processing(data: np.ndarray) -> np.ndarray:
     diff_data_norm = diff_data / np.max(diff_data)
     diff_data_clip = np.where(diff_data_norm > .4, diff_data_norm, 0)
     return sg.find_peaks(diff_data_clip)[0]
+
+def calculate_bpm(peaks: np.ndarray, sampling_rate: int, start_timestamp: float) -> list:
+    bpm_w_timestamps = []
+
+    current_timestamp = start_timestamp
+    for i in range(1, peaks.size):
+        if i + 1 != peaks.size:
+            current_timestamp += 1 / sampling_rate
+            bpm = sampling_rate / (peaks[i + 1] - peaks[i]) * 60
+            bpm_w_timestamps.append((current_timestamp, int(np.round(bpm))))
+            
+    return bpm_w_timestamps
